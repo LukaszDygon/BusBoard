8fff8725baec4b4796f642345e2f175bc9025481
LukaszDygon <ld13281@my.bristol.ac.uk>
2017-09-11 11:33:03 +0100

    Very beginning

--- BusBoard.ConsoleApp/Program.cs
+++ BusBoard.ConsoleApp/Program.cs
@@ -3,13 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using RestSharp;
 
 namespace BusBoard.ConsoleApp
 {
-  class Program
-  {
-    static void Main(string[] args)
+    class Program
     {
+        static void Main(string[] args)
+        {
+            string stopId = "490008660N";
+            PrintBusArivalsForStop(stopId);
+            Console.ReadKey();
+        }
+
+        static void PrintBusArivalsForStop(string stopId)
+        {
+            var client = new RestClient("https://api-radon.tfl.gov.uk/");
+            //var request = new RestRequest(String.Format(@"StopPoint/{0}/Arrivals", stopId), Method.GET);
+            var request = new RestRequest("StopPoint/490008660N/Arrivals", Method.GET);
+            IRestResponse response = client.Execute(request);
+
+            Console.WriteLine(response.StatusCode);
+        }
     }
-  }
 }
